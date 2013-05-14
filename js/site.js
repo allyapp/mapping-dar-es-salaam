d2e48643987ea315bbf20e8bc938c68817c50863
Tom MacWright <tom@macwright.org>
2013-05-14 13:42:56 -0400

    Add video as placeholder

--- js/site.js
+++ js/site.js
@@ -1,24 +1,31 @@
 var cartogram_image = d3.select('.cartogram-image');
 
-var spriteScale = d3.scale.linear()
-    .domain([0, 1])
-    .range([0, 32768]);
+function bgTween(to) {
+    return function() {
+        var spriteScale = d3.scale.linear()
+            .domain([0, 1])
+            .range([0, to * 512]);
 
-function bgTween() {
-    return function(i) {
-        return (-Math.floor(spriteScale(i) / 512) * 512) + 'px 0px';
+        return function(i) {
+            return (-Math.floor(spriteScale(i) / 512) * 512) + 'px 0px';
+        };
     };
 }
 
-function clickYear() {
-    d3.select(this).text();
+var yrs = [0, 20, 37, 44, 64];
+
+function clickYear(d, i) {
+    var yr = d3.select(this).text();
     cartogram_image
         .transition()
-        .duration(10000)
+        .duration(1000)
         .ease('linear')
-        .styleTween('background-position', bgTween);
+        .styleTween('background-position', bgTween(yrs[i]));
 }
 
 d3.select('#regions')
     .selectAll('h3')
+    .on('click', clickYear);
+
+d3.selectAll('.cartogram span')
     .on('click', clickYear);
