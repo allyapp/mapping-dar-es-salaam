848be040307364123d1bf4a07e9d65e842a0ca5d
Tom MacWright <tom@macwright.org>
2013-05-14 13:20:31 -0400

    Rework again

--- js/site.js
+++ js/site.js
@@ -1,28 +1,24 @@
-var $notes = $('.cartogram-notes');
-var $frame = $('.cartogram-frame');
-$notes.find('p').hide();
+var cartogram_image = d3.select('.cartogram-image');
 
-function lpad(n, padto) {
-    while (('' + n).length < padto) n = '0' + n;
-    return n;
+var spriteScale = d3.scale.linear()
+    .domain([0, 1])
+    .range([0, 32768]);
+
+function bgTween() {
+    return function(i) {
+        return (-Math.floor(spriteScale(i) / 512) * 512) + 'px 0px';
+    };
 }
 
-function findRange(frame) {
-    var ranges = [0, 20, 37, 44, 65];
-    for (var i = 0; i < ranges.length; i++) {
-        if (ranges[i + 1] === undefined || ranges[i + 1] > frame) {
-            return i;
-        }
-    }
+function clickYear() {
+    d3.select(this).text();
+    cartogram_image
+        .transition()
+        .duration(10000)
+        .ease('linear')
+        .styleTween('background-position', bgTween);
 }
 
-new Dragdealer('cartogram-slider', {
-    horizontal: true,
-    vertical: false,
-    animationCallback: function(x, y) {
-        var fn = Math.round(x * 64 + 1);
-        $notes.find('p').hide();
-        $notes.find('p').eq(findRange(fn)).show();
-        $frame.attr('src', 'frames/' + lpad(fn, 3) + '.ppm.gif');
-    }
-});
+d3.select('#regions')
+    .selectAll('h3')
+    .on('click', clickYear);
